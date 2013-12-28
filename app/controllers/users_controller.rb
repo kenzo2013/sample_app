1e5517a7815ee28b9bed4dbf60b5abe4894187f8
noumedem <kkenzo2007@yahoo.fr>
2013-12-28 19:54:48 +0100

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   def new
-  @titre = "Inscription"
+  	@titre = "Inscription"
+  	@user = User.new
   end
   def show
 	@user = User.find(params[:id])
 	@titre = @user.name
   end
+  def create
+ 	 @user = User.new(user_params)
+    if @user.save
+    	flash[:success] = "Bienvenue dans l'Application Exemple!"
+  	redirect_to @user
+  	  else
+  		@titre = "Inscription"
+  		render 'new'
+     end
+  end
   # Never trust parameters from the scary internet, only allow the white list through.
-    def actor_params
+    def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
